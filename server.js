dab6263269c36588668384ea132f219cb4080fed
AminahBella <mmakgatiaminah41@gmail.com>
2026-05-19 10:43:32 +0200

    Fixing branch conflicts

--- server.js
+++ server.js
@@ -3,23 +3,27 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
+const urlRoutes = require('./redirect/urlRedirect');
+
 dotenv.config();
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MongoURL, )
-.then(() => {
-    console.log('MongoDB Connected');
-})
-.catch((error) => {
-    console.log(error);
-});
+// routes
+app.use('/', urlRoutes);
 
+// DB connect
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// test route
 app.get('/', (req, res) => {
-    res.send('Backend Server Running');
+    res.send("URL Shortener API Running");
 });
 
 const PORT = process.env.PORT || 5000;
